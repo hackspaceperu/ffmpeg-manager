604bc8be2d93a83ff6f2de2b69ad43048f40123b
Luis Chunga Rios <luischungar7@gmail.com>
2019-06-22 02:24:14 -0500

    config

--- functions/restify/index.js
+++ functions/restify/index.js
@@ -1,13 +1,23 @@
 const restify = require('restify')
+const corsMiddleware = require('restify-cors-middleware')
 const server = restify.createServer({
   name : 'ffmpeg-manager'
 })
+
+const cors = corsMiddleware({
+ 
+  origins: ['*'],
+  allowHeaders: [],
+  exposeHeaders: ['']
+})
+
+server.pre(cors.preflight)
+server.use(cors.actual)
 
 server.listen(process.env.APP_PORT, () => {
   console.log('ready on ', server.url)
 })
 
 server.get('/', (req, res ) => {
-  res.send('Gooo ')
+  res.send('Gooo xd ')
 })
-
