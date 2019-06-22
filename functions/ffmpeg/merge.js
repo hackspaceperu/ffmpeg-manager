663db7cf018b91b23686e236fc9a878d8d810551
Dante Calderon <dantehemerson@gmail.com>
2019-06-22 15:00:12 -0500

    Check if file  is valid

--- functions/ffmpeg/merge.js
+++ functions/ffmpeg/merge.js
@@ -48,29 +48,25 @@ export function ffprobe(file) {
 
     const opts = {}
 
-    /* const cb = (error, stdout) => {
-        if (error) reject(error);
-  
-        const result = JSON.parse(stdout)
-  
-        if (!result.streams) return reject('This file has no streams');
-  
-        const isValidFile = result.streams.some(({codec_type, duration}) =>
-          (codec_type === 'video' || codec_type === 'audio')
-        )
-  
-        if (!isValidFile) {
-          return reject('FFprobe: no valid media stream found')
-        } else {
-          console.log('Valid file found. FFProbe finished')
-          return resolve(result)
-        }
-      } */
-    const child = spawn('ffprobe', args, opts)
-      .on('message', msg => console.log(msg))
-      .on('error', reject)
+    const cb = (error, stdout) => {
+      if (error) reject(error)
 
-    child.stdout.on('data', data => resolve(process.stdout.write(data)))
-    child.stderr.on('data', data => reject(process.stdout.write(data)))
+      const result = JSON.parse(stdout)
+
+      if (!result.streams) return reject('This file has no streams')
+
+      const isValidFile = result.streams.some(
+        ({ codec_type, duration }) =>
+          codec_type === 'video' || codec_type === 'audio'
+      )
+
+      if (!isValidFile) {
+        return reject('FFprobe: no valid media stream found')
+      } else {
+        console.log('Valid file found. FFProbe finished')
+        return resolve(result)
+      }
+    }
+    const child = execFile('ffprobe', args, opts, cb).on('error', reject)
   })
 }
--- functions/restify/server/Controllers/managerController.js
+++ functions/restify/server/Controllers/managerController.js
@@ -1,7 +1,7 @@
 const FmmpegController = require('./ffmpegController')
 const StorageController = require('./storageController')
 import { NotFoundException } from '../Responses/Exceptions/NotFoundException'
-import { isUrl } from '../../../../utils'
+import { isUrl, absolutePath } from '../../../../utils'
 import { ffprobe, ffmpeg } from '../../../ffmpeg/merge'
 
 export class ManagerController {
@@ -14,26 +14,29 @@ export class ManagerController {
     if (!files || !settings) {
       throw new NotFoundException('Missing files or settings.')
     }
-    const desPaths = await Promise.all(
-      files.map(
-        filePath =>
-          new Promise((resolve, reject) => {
-            // Llamar a download
-            if (isUrl(filePath)) {
-              resolve('Es url')
-            } else {
-              resolve('Noes url')
-            }
-          })
-      )
-    )
+    // const desPaths = await Promise.all(
+    //   files.map(
+    //     filePath =>
+    //       new Promise((resolve, reject) => {
+    //         // Llamar a download
+    //         if (isUrl(filePath)) {
+    //           resolve('Es url')
+    //         } else {
+    //           resolve('Noes url')
+    //         }
+    //       })
+    //   )
+    // )
 
+    const desPaths = ['./min2i.webm', 'alksajdfk']
     try {
       const results = await Promise.all(
-        desPaths.map(destPath => ffprobe(desPaths))
+        desPaths.map(destPath => ffprobe(destPath))
       )
+      console.log('finish')
       console.log(results)
     } catch (e) {
+      console.log('error')
       throw new NotFoundException(e.message)
     }
 
