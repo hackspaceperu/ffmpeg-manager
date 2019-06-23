e493dbbbc7afc85d1d7fd7f38967baa08aa52c9f
Dante Calderon <dantehemerson@gmail.com>
2019-06-22 20:32:39 -0500

    Fix args

--- functions/ffmpeg/merge.js
+++ functions/ffmpeg/merge.js
@@ -2,7 +2,7 @@ import { path } from 'path'
 import { outputDirectory, mergeArgs, mergeInputFiles } from '../../utils'
 import { spawn, execFile } from 'child_process'
 
-export function ffmpeg(file, ext="webm", ffmpegArgs) {
+export function ffmpeg(file, ext = 'webm', ffmpegArgs) {
   return new Promise((resolve, reject) => {
     const optDirectory = outputDirectory('mergedVideo', ext)
     const args = [
@@ -61,8 +61,8 @@ export function ffprobe(file) {
       } else {
         console.log('Valid file found. FFProbe finished')
         let o = {
-          'start': parseInt(result.format.start_time),
-          'duration': parseInt(result.format.duration),
+          start: parseInt(result.format.start_time),
+          duration: parseInt(result.format.duration)
         }
         return resolve(o)
       }
--- functions/restify/server/Controllers/ffmpegController.js
+++ functions/restify/server/Controllers/ffmpegController.js
@@ -22,24 +22,27 @@ export class FfmpegController {
       throw new NotFoundException('No hay 4 videos.')
     }
 
-    let args = [
-      '-filter_complex',
-      '"nullsrc=size=640x480 [base];' +
-        '[0:v] setpts=PTS-STARTPTS, scale=213x240 [upperleft];' +
-        '[1:v] setpts=PTS-STARTPTS, scale=213x240 [uppermiddle];' +
-        '[2:v] setpts=PTS-STARTPTS, scale=213x240 [upperleft];' +
-        '[3:v] setpts=PTS-STARTPTS, scale=213x240 [lowerleft];' +
-        '[4:v] setpts=PTS-STARTPTS, scale=213x240 [lowermiddle];' +
-        '[5:v] setpts=PTS-STARTPTS, scale=213x240 [lowerright];' +
-        '[base][upperleft] overlay=shortest=1 [tmp1];' +
-        '[tmp1][uppermiddle] overlay=shortest=1:x=213 [tmp2];' +
-        '[tmp2][upperleft] overlay=shortest=1:x=426 [tmp3];' +
-        '[tmp3][lowerleft] overlay=shortest=1:y=240 [tmp4];' +
-        '[tmp4][lowermiddle] overlay=shortest=1:y=240:x=213 [tmp5];' +
-        '[tmp5][lowerright] overlay=shortest=1:y=240:x=426"',
-      `-r ${fps}`
-    ]
+    // let args = [
+    //   '-filter_complex',
+    //   '"nullsrc=size=640x480 [base];' +
+    //     '[0:v] setpts=PTS-STARTPTS, scale=213x240 [upperleft];' +
+    //     '[1:v] setpts=PTS-STARTPTS, scale=213x240 [uppermiddle];' +
+    //     '[2:v] setpts=PTS-STARTPTS, scale=213x240 [upperleft];' +
+    //     '[3:v] setpts=PTS-STARTPTS, scale=213x240 [lowerleft];' +
+    //     '[4:v] setpts=PTS-STARTPTS, scale=213x240 [lowermiddle];' +
+    //     '[5:v] setpts=PTS-STARTPTS, scale=213x240 [lowerright];' +
+    //     '[base][upperleft] overlay=shortest=1 [tmp1];' +
+    //     '[tmp1][uppermiddle] overlay=shortest=1:x=213 [tmp2];' +
+    //     '[tmp2][upperleft] overlay=shortest=1:x=426 [tmp3];' +
+    //     '[tmp3][lowerleft] overlay=shortest=1:y=240 [tmp4];' +
+    //     '[tmp4][lowermiddle] overlay=shortest=1:y=240:x=213 [tmp5];' +
+    //     '[tmp5][lowerright] overlay=shortest=1:y=240:x=426"',
+    //   `-r ${fps}`
+    // ]
 
+    let args = `-filter_complex "nullsrc=size=640x480 [base]; [0:v] setpts=PTS-STARTPTS, scale=213x240 [upperleft]; [1:v] setpts=PTS-STARTPTS, scale=213x240 [uppermiddle]; [2:v] setpts=PTS-STARTPTS, scale=213x240 [upperleft]; [3:v] setpts=PTS-STARTPTS, scale=213x240 [lowerleft]; [4:v] setpts=PTS-STARTPTS, scale=213x240 [lowermiddle]; [5:v] setpts=PTS-STARTPTS, scale=213x240 [lowerright]; [base][upperleft] overlay=shortest=1 [tmp1]; [tmp1][uppermiddle] overlay=shortest=1:x=213 [tmp2]; [tmp2][upperleft] overlay=shortest=1:x=426 [tmp3]; [tmp3][lowerleft] overlay=shortest=1:y=240 [tmp4]; [tmp4][lowermiddle] overlay=shortest=1:y=240:x=213 [tmp5]; [tmp5][lowerright] overlay=shortest=1:y=240:x=426" -r ${fps}`
+
+    args = args.split(' ')
     // const args = [
     //   '-filter_complex',
     //   `hstack=inputs=4`,
